cd75816658a28a004662705e8ca1e254d7ba4f57
Alex Lee and Chris Wainner <alex+chris@>
2017-03-21 09:29:08 -0700

    to do List

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,7 +2,12 @@ import java.util.Map;
 import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static spark.Spark.*;
+
 
 public class App {
   public static void main(String[] args) {
@@ -11,8 +16,31 @@ public class App {
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
+      model.put("tasks", request.session().attribute("tasks"));
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/tasks", (request, response) -> {
+     Map<String, Object> model = new HashMap<String, Object>();
+
+     ArrayList<Task> tasks = request.session().attribute("tasks");
+     if (tasks == null) {
+       tasks = new ArrayList<Task>();
+       request.session().attribute("tasks", tasks);
+     }
+
+    //  ArrayList<String> task2 = new ArrayList<String>();
+    //  task2.add("asd");
+
+     String description = request.queryParams("description");
+     Task newTask = new Task(description);
+     tasks.add(newTask);
+
+     model.put("tasks",tasks);
+     model.put("template", "templates/success.vtl");
+
+     return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
   }
